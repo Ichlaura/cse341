ebb723c92778762e435dccb83b42f730e334802f
laura nunez <laupao41@gmail.com>
2025-09-10 14:18:22 +0900

    arreglo de contacts.js

--- cse341-contacts/routes/contacts.js
+++ cse341-contacts/routes/contacts.js
@@ -1,12 +1,12 @@
 const express = require('express');
 const contactsController = require('../controllers/contacts');
-
 const router = express.Router();
 
-// GET all contacts
 router.get('/', contactsController.getAllContacts);
-
-// GET one contact by ID
 router.get('/:id', contactsController.getContactById);
+router.post('/', contactsController.createContact);
+router.put('/:id', contactsController.updateContact);
+router.patch('/:id', contactsController.partialUpdateContact);
+router.delete('/:id', contactsController.deleteContact);
 
 module.exports = router;
