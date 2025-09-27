61f4b4550996b184a9aa0ee8ab37a1cc4ede3f2d
laura nunez <laupao41@gmail.com>
2025-09-28 02:49:22 +0900

    Use manual swagger file, remove swagger-autogen

--- cse341-contacts/src/app.js
+++ cse341-contacts/src/app.js
@@ -1,33 +1,42 @@
-const swaggerAutogen = require('swagger-autogen')();
-
-const doc = {
-  info: {
-    title: 'Contacts API',
-    description: 'Contacts API'
-  },
-  host: 'cse341-kzf0.onrender.com',
-  schemes: ['https']
-};
-
-
-
+require('dotenv').config();
+const express = require('express');
+const bodyParser = require('body-parser');
+const mongodb = require('../db/connect');
+const contactsRoutes = require('../routes/contacts');
 const swaggerUi = require('swagger-ui-express');
-let swaggerDocument;
+const swaggerDocument = require('./swagger-output.json');
+const cors = require('cors');
 
-try {
-  swaggerDocument = require('./swagger-output.json');
-} catch (error) {
-  console.log('Swagger file not found, using basic setup');
-  swaggerDocument = {
-    info: {
-      title: 'Contacts API',
-      description: 'Contacts API'
-    }
-  };
-}
+const app = express();
+const port = process.env.PORT || 8081;
 
-app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument)); 
-const outputFile = './src/swagger-output.json';
-const endpointsFiles = ['./routes/contacts.js'];
+app.use(bodyParser.json());
+app.use(cors());
 
-swaggerAutogen(outputFile, endpointsFiles, doc);
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+  next();
+});
+
+// Swagger documentation
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+
+// Routes
+app.use('/contacts', contactsRoutes);
+
+app.get('/test', (req, res) => {
+  res.json({ message: 'API working', timestamp: new Date() });
+});
+
+app.get('/', (req, res) => {
+  res.send('API de Contacts funcionando');
+});
+
+// MongoDB connection
+mongodb.initDb()
+  .then(() => {
+    app.listen(port, () => console.log(`Server running on port ${port}`));
+  })
+  .catch(err => console.error('Failed to start server', err));
