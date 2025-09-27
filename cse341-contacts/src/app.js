aa3e78bd1fe8d523a13ecc40d160c1c6be653930
laura nunez <laupao41@gmail.com>
2025-09-28 02:47:26 +0900

    Use manual swagger file

--- cse341-contacts/src/app.js
+++ cse341-contacts/src/app.js
@@ -9,6 +9,24 @@ const doc = {
   schemes: ['https']
 };
 
+
+
+const swaggerUi = require('swagger-ui-express');
+let swaggerDocument;
+
+try {
+  swaggerDocument = require('./swagger-output.json');
+} catch (error) {
+  console.log('Swagger file not found, using basic setup');
+  swaggerDocument = {
+    info: {
+      title: 'Contacts API',
+      description: 'Contacts API'
+    }
+  };
+}
+
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument)); 
 const outputFile = './src/swagger-output.json';
 const endpointsFiles = ['./routes/contacts.js'];
 
